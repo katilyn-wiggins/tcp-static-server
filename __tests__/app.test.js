f32cbf99fdd14636db12536c5fd9372636e7fa39
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-31 13:35:51 -0300

    connection sucessful

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,11 @@ const app = require('../lib/app');
 const index = require('../public/index.html');
 
 describe('app routes', () => {
+  it('ASYNC/AWAIT: it returns hi when it sends a get request to the / endpoint', async () => {
+    const res = await request(app).get('/');
+
+    expect(res.text).toEqual('hi');
+  });
   it('ASYNC/AWAIT: it returns index.html from our public directory when it sends a request to the /index.html endpoint', async () => {
     const res = await request(app).get('/index.html');
 

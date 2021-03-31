dea029fb7bf2e37e23fedf4e123c98c22b838f80
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-31 14:32:24 -0300

    index test passing yay

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const index = require('../public/index.html');
+// const index = require('../public/index.html');
 
 describe('app routes', () => {
   it('ASYNC/AWAIT: it returns hi when it sends a get request to the / endpoint', async () => {
@@ -11,6 +11,35 @@ describe('app routes', () => {
   it('ASYNC/AWAIT: it returns index.html from our public directory when it sends a request to the /index.html endpoint', async () => {
     const res = await request(app).get('/index.html');
 
-    expect(res.text).toEqual(index);
+    expect(res.text).toEqual(`<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Document</title>
+</head>
+<body>
+<h7>One fine day with a woof and a purr
+A baby was born and it caused a little stir
+No blue buzzard, no three-eyed frog
+Just a feline, canine little catdog
+Catdog
+Catdog
+Alone in the world was a little catdog
+
+Out on the road, or back in town
+All kinda critters putting catdog down
+Gotta rise above it, gotta try to get along
+Gotta work together, gotta sing this song
+Catdog
+Catdog
+Alone in the world was a little catdog
+
+Catdog
+Catdog
+Alone in the world was a little catdog
+Alone in the world was a little catdog</h7>
+</body>
+</html>`);
   });
 });
--- node-server.js
+++ node-server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./lib/app');
 
-http
-  .createServer((req, res) => {
-    res.end('hello');
-  })
-  .listen(7890);
+http.createServer((req, res) => {
+  res.end('hello');
+});
+app.listen(7891);
